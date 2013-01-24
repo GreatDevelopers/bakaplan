d9534d73a6f724d34af7cb225a599229f66488fb
megha55 <meghasimak@gmail.com>
2013-01-25 01:13:24 +0530

    path changed

--- file-names.h
+++ file-names.h
@@ -1,8 +1,8 @@
 // file names
 
-const int FILE_SIZE = 35;
+const int FILES_SIZE = 35;
 
-const char BranchDetails_in[FILE_SIZE]   =   "SeatPlan/BranchDetails.in";
-const char TotalFields[FILE_SIZE]        =   "TotalFields.txt";
-const char RollNoDetails_in[FILE_SIZE]   =   "SeatPlan/RollNoDetails.in";
-const char RoomDetails_in[FILE_SIZE]     =   "seatPlan/RoomDetails.in";
+const char BranchDetails_in[FILES_SIZE]   =   "SeatPlan/BranchDetails.in";
+const char TotalFields[FILES_SIZE]        =   "TotalFields.txt";
+const char RollNoDetails_in[FILES_SIZE]   =   "SeatPlan/RollNoDetails.in";
+const char RoomDetails_in[FILES_SIZE]     =   "SeatPlan/RoomDetails.in";
