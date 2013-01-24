6cef9c9b3f26d95068eeb91d2babefaa5e6ef6c2
megha55 <meghasimak@gmail.com>
2013-01-25 01:29:03 +0530

    file names

--- SeatPlan/file-names.h
+++ SeatPlan/file-names.h
@@ -1,31 +1,40 @@
 // This file includes all file names which are creating in seating plan process.
 
 // Constant File name size
-const int FILE_SIZE = 95;
+const int FILE_SIZE                         = 40;
 
 // For checking purpose
-const char Checking[FILE_SIZE] = "Checking.txt";
+const char Checking[FILE_SIZE]              = "Checking.txt";
 
 // Input Files
-//string Input_Rollno = "rollno.in";
-const char Input_Rollno[FILE_SIZE] = "RollNoDetails.in";//"rollno.in";
-const char Input_BranchDetails[FILE_SIZE] = "BranchDetails.in"; //"branchdetails.in";
-const char Input_Rooms[FILE_SIZE] = "RoomDetails.in"; //"rooms.in";
-
-//const char Input_Rollno[FILE_SIZE] = "rollno.in";
-//const char Input_BranchDetails[FILE_SIZE] = "branchdetails.in";
-//const char Input_Rooms[FILE_SIZE] = "rooms.in";
+const char Input_Rollno[FILE_SIZE]          = "SeatPlan/RollNoDetails.in";
+const char Input_BranchDetails[FILE_SIZE]   = "SeatPlan/BranchDetails.in";
+const char Input_Rooms[FILE_SIZE]           = "SeatPlan/RoomDetails.in";
 
 // Roll no Processing 
-const char Rollno_Expand_out[FILE_SIZE] = "rollno_expand.out";
-const char Rollno_Arranged_out[FILE_SIZE] = "rollno_arranged.out";
+const char Rollno_Expand_out[FILE_SIZE]     = "SeatPlan/rollno_expand.out";
+const char Rollno_Arranged_out[FILE_SIZE]   = "SeatPlan/rollno_arranged.out";
 
 // Re-arranged i/p files
-//const char Input_Rooms_out[FILE_SIZE] = "input_rooms.out";
-const char Input_Rollno_out[FILE_SIZE] = "input_rollno.out";
+const char Input_Rollno_out[FILE_SIZE]      = "SeatPlan/input_rollno.out";
 
 // Subject wise roll nos.
-const char Subjectwise_Rollno_out[FILE_SIZE] = "subject_rollno.out";
+const char Subjectwise_Rollno_out[FILE_SIZE]= "SeatPlan/subject_rollno.out";
+
+// used in strategy programs
+
+const char Strategy_Results[FILE_SIZE]      = "StrategyResults.out";
+const char Seat_Plan_out[FILE_SIZE]         = "Seat-Plan.out";
+
+// Report Files
+
+const char SeatPlan_TextFile[FILE_SIZE]     = "SeatPlan.txt";
+const char SeatPlan_HTMLFile[FILE_SIZE]     = "SeatPlan.html";
+const char SeatPlan_CSVFile[FILE_SIZE]      = "SeatPlan.csv";
+const char SeatPlan_PDFFile[FILE_SIZE]      = "SeatPlan.pdf";
+const char BranchReportTextFile[FILE_SIZE]  = "BranchReport.txt";
+const char BranchReportPDFFile[FILE_SIZE]   = "BranchReport.pdf";
+const char BranchReportHTMLFile[FILE_SIZE]  = "BranchReport.html";
 
 /*// Strategy 
 const char Strategy_Results_out[FILE_SIZE] = "strategy_results.out";
@@ -44,17 +53,4 @@ const char BranchReport_TextFile[FILE_SIZE] = "branch_report.txt";
 const char BranchReport_PDFFile[FILE_SIZE] = "branch_report.pdf";
 const char BranchReport_HTMLFile[FILE_SIZE] = "branch_report.html";*/
 
-// used in strategyVal programs
 
-const char strategyResults[25] = "strategyResults.txt";
-const char Seat_Plan_out[25] = "Seat-Plan.out";
-
-// Report Files
-
-const char TextFile[25] = "SeatPlan.txt";
-const char HTMLFile[25] = "SeatPlan.html";
-const char CSVFile[25] = "SeatPlan.csv";
-const char PDFFile[25] = "SeatPlan.pdf";
-const char BranchReportTextFile[25] = "BranchReport.txt";
-const char BranchReportPDFFile[25] = "BranchReport.pdf";
-const char BranchReportHTMLFile[25] = "BranchReport.html";
--- file-names.h
+++ file-names.h
@@ -2,7 +2,8 @@
 
 const int FILES_SIZE = 35;
 
-const char BranchDetails_in[FILES_SIZE]   =   "SeatPlan/BranchDetails.in";
-const char TotalFields[FILES_SIZE]        =   "TotalFields.txt";
-const char RollNoDetails_in[FILES_SIZE]   =   "SeatPlan/RollNoDetails.in";
-const char RoomDetails_in[FILES_SIZE]     =   "SeatPlan/RoomDetails.in";
+const char BranchDetails_in[FILES_SIZE]     =   "SeatPlan/BranchDetails.in";
+const char TotalFields[FILES_SIZE]          =   "TotalFields.txt";
+const char RollNoDetails_in[FILES_SIZE]     =   "SeatPlan/RollNoDetails.in";
+const char RoomDetails_in[FILES_SIZE]       =   "SeatPlan/RoomDetails.in";
+const char Strategy_in[FILES_SIZE]          =   "SeatPlan/Strategy.in";       
