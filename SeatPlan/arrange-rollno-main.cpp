e543af82211009a1b1fe80c363ea029dce0170a0
megha55 <meghasimak@gmail.com>
2013-01-25 00:25:37 +0530

    Backend Proceccing

---
+++ SeatPlan/arrange-rollno-main.cpp
@@ -0,0 +1,9 @@
+#include "arrange-rollno.h"
+
+int main(void)
+{
+   ArrangeRollNo arrange;
+   arrange.Main();
+   
+   return 0;
+}
---
+++ SeatPlan/read-input-main.cpp
@@ -0,0 +1,9 @@
+#include "read-input.h"
+
+int main(void)
+{
+   ReadInput read;
+   read.Main();
+   
+   return 0;
+}
