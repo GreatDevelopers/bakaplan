342490a01f4700e07b021e4b1b2e67b2bee68016
megha55 <meghasimak@gmail.com>
2013-02-15 19:49:19 +0530

    Function Definition for PageLayout Class

---
+++ src/pagelayout.cc
@@ -0,0 +1,21 @@
+/**
+ * ===================================================================
+ *
+ *       Filename:  pagelayout.cc
+ *
+ *    Description:  Definition of member functions of PageLayout
+ *    class. This includes header, footer section of html pages.
+ *
+ *        Version:  0.6
+ *        Created:  Friday 15 February 2013 07:05:50  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/// Include Header file
+#include "pagelayout.h"
