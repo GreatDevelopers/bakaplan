0f32ca5d43ce54c502a3e032d17e74a2db5a8106
megha55 <meghasimak@gmail.com>
2013-02-15 17:06:38 +0530

    source files for html pages

---
+++ src/header.h
@@ -0,0 +1,28 @@
+/**
+ * ===================================================================
+ *
+ *       Filename:  header.h
+ *
+ *    Description:  Header file for including commom headerfiles.
+ *
+ *        Version:  0.6
+ *        Created:  Friday 15 February 2013 04:53:17  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+\\\ Constant variables(Global)
+
+const int MIN_SIZ = 30;
+const int MAX_SIZ = 50;
