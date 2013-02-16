417d0f896a8a3a86d7d460ba23a6427c9fa2a3d9
megha55 <meghasimak@gmail.com>
2013-02-17 01:18:12 +0530

    I/O file names added

--- src/filenames.h
+++ src/filenames.h
@@ -1,7 +1,7 @@
 /*
  * ===================================================================
  *
- *       Filename:  file-names.h
+ *       Filename:  filenames.h
  *
  *    Description:  This file includes names of input and output
  *    files.
@@ -17,4 +17,25 @@
  * ===================================================================
  */
 
+/**-------------------------------------------------------------------
+ *  Constant Names of I/p and O/P files
+ *------------------------------------------------------------------*/
 
+const int FILES_SIZE = 35;
+
+/**-------------------------------------------------------------------
+ *  Input File Names
+ *------------------------------------------------------------------*/
+
+const char CLASS_DETAILS_IN[FILES_SIZE]    =   "class-details.in";
+const char TOTAL_FIELDS[FILES_SIZE]        =   "total-fields.txt";
+const char ROLLNO_DETAILS_IN[FILES_SIZE]   =   "rollno-details.in";
+const char ROOM_DETAILS_IN[FILES_SIZE]     =   "room-details.in";
+const char STRATEGY_IN[FILES_SIZE]         =   "strategy.in";     
+const char EXAM_DETAILS_IN[FILES_SIZE]     =   "exam-details.in";
+
+/**-------------------------------------------------------------------
+ *  Output File Names
+ *------------------------------------------------------------------*/
+
+const char VALIDATION_OUT[FILES_SIZE]      =   "validation.out";
