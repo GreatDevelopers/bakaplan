6b7f97727f06eb2c086cefc5a9173f2fe3d783fc
megha55 <meghasimak@gmail.com>
2013-02-16 23:22:58 +0530

    RoonDetails Class Declaration and definition

---
+++ src/roomdetails.cc
@@ -0,0 +1,20 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  roomdetails.cc
+ *
+ *    Description:  Definition of member functions of RoomDetails
+ *    Class.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 11:19:40  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
---
+++ src/roomdetails.h
@@ -0,0 +1,21 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  roomdetails.h
+ *
+ *    Description:  Declaration of RoomDetails Class for getting
+ *    information about rooms, its rows and columns, also total
+ *    centres and its names.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 11:18:34  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
