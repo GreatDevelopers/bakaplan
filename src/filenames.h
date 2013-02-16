4d8c6a3062e20beaaf061c14989f2dab6bf605a1
megha55 <meghasimak@gmail.com>
2013-02-16 23:21:18 +0530

    names of input and o/p files

---
+++ src/filenames.h
@@ -0,0 +1,20 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  file-names.h
+ *
+ *    Description:  This file includes names of input and output
+ *    files.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 02:24:08  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
