f6ecac39dd96958026634c428687eb5b099cb767
megha55 <meghasimak@gmail.com>
2013-02-16 23:23:39 +0530

    RedInputFields Class definition and declaration

---
+++ src/readinputfields.cc
@@ -0,0 +1,19 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  read-input-fields.cc
+ *
+ *    Description:  Definition of ReadInputFields's member functions.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 02:32:31  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
---
+++ src/readinputfields.h
@@ -0,0 +1,20 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  readinputfields.h
+ *
+ *    Description:  Declaration of ReadInputField ie used to read
+ *    value of any field using cgicc library.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 02:31:27  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
