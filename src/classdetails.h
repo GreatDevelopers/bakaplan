c11110fffcbb0cdd01fa3839902ad15e7c5674a3
megha55 <meghasimak@gmail.com>
2013-02-16 23:20:44 +0530

    ClassDetails Class Declaration and Definition

---
+++ src/classdetails.h
@@ -0,0 +1,21 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  classdetails.h
+ *
+ *    Description:  Declaration of ClassDetails class to getting
+ *    details like total classes, classname, subjects its code and
+ *    name, etc.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 11:14:34  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
