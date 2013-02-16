4d049bf95fcdd4ec92318e9ba9d7120c92e6183f
megha55 <meghasimak@gmail.com>
2013-02-16 23:22:00 +0530

    InputFieldNames class declaration and definition

---
+++ src/inputfieldnames.cc
@@ -0,0 +1,19 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  input-field-names.cc
+ *
+ *    Description:  Definition of InputFieldNames's member functions.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 02:29:48  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
---
+++ src/inputfieldnames.h
@@ -0,0 +1,20 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  input-field-names.h
+ *
+ *    Description:  Declaration of InputFieldNames Class ie used to
+ *    store input field names.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 02:28:58  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
