1be7068222520ce3c40732535fb6c08d345a1eb8
megha55 <meghasimak@gmail.com>
2013-02-16 23:22:28 +0530

    RollNoDetails class declation and Definition

---
+++ src/rollnodetails.cc
@@ -0,0 +1,19 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  rollnodetails.cc
+ *
+ *    Description:  Definition of RollNoDetails's member functions.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 11:17:41  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
---
+++ src/rollnodetails.h
@@ -0,0 +1,20 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  rollnodetails.h
+ *
+ *    Description:  Declaration of RollNoDetails class for getting
+ *    roll no details of classes.
+ *
+ *        Version:  0.6
+ *        Created:  Saturday 16 February 2013 11:17:00  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+
