298894d94b6c8bd8343df95f34bbe15421714730
megha55 <meghasimak@gmail.com>
2013-02-18 14:48:16 +0530

    main func to get total classes fron user through cgi

---
+++ src/totalclasses-main.cpp
@@ -0,0 +1,37 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  totalclasses-main.cpp
+ *
+ *    Description:  Includes main method, read value from user through
+ *    html page by creating object of ClassDetails class.
+ *
+ *        Version:  0.6
+ *        Created:  Monday 18 February 2013 02:36:10  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  Include classdetails.h header file
+ *------------------------------------------------------------------*/
+
+#include "classdetails.h"
+
+
+/**-------------------------------------------------------------------
+ *  Main Method
+ *------------------------------------------------------------------*/
+
+ int main(void)
+ {
+     ClassDetails class;
+     class.TotalClasses();
+
+     return 0;
+ }
