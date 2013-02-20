86e0542be901f920a62e56b584f063551d1ed5c2
megha55 <meghasimak@gmail.com>
2013-02-20 17:35:32 +0530

    class details)

---
+++ src/classinfo-main.cpp
@@ -0,0 +1,36 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  classinfo.cpp
+ *
+ *    Description:  Main method file for getting class info like class
+ *    name, subject code and subject names required for seating plan.
+ *
+ *        Version:  0.6
+ *        Created:  Wednesday 20 February 2013 03:20:37  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  Include classdetails.h header file for calling ClassInfo function  
+ *------------------------------------------------------------------*/
+
+#include "classdetails.h"
+                                                
+/**-------------------------------------------------------------------
+ *  Main method
+ *------------------------------------------------------------------*/
+
+int main(void)
+{
+   ClassDetails classInfo;
+   classInfo.ClassInfo();
+
+   return 0;
+}
