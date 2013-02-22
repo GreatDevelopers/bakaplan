fb3c75149fc08e2665178f23a4836d2a22e18d52
megha55 <meghasimak@gmail.com>
2013-02-22 12:10:43 +0530

    roll no details

--- src/rollnodetails.cc
+++ src/rollnodetails.cc
@@ -16,4 +16,66 @@
  * ===================================================================
  */
 
+/**-------------------------------------------------------------------
+ *  Include rollnodetails.h for RollNoDetails class declaration
+ *------------------------------------------------------------------*/
 
+#include "rollnodetils.h"
+
+/**-------------------------------------------------------------------
+ *  Definition of functions RollNoDetails Class
+ *------------------------------------------------------------------*/
+
+/**
+ *--------------------------------------------------------------------
+ *       Class:  RollNoDetails
+ *      Method:  RollNoDetails :: RollNoDetails()
+ * Description:  Constructor
+ *--------------------------------------------------------------------
+ */
+
+RollNoDetails :: RollNoDetails()
+{
+
+}
+
+/**
+ *--------------------------------------------------------------------
+ *       Class:  RollNoDetails
+ *      Method:  RollNoDetails :: ReadClassDetails()
+ * Description:  For reading class details from previous page
+ *--------------------------------------------------------------------
+ */
+
+void RollNoDetails :: ReadClassDetails()
+{
+   temp = readField.ReadFieldValue(fieldName.totalClasses);
+   totalClasses = readField.StringToIn(temp);
+
+   for(i = 0; i < totalClasses; i++)
+   {
+       prefix[i] = readField.ReadFieldValue(fieldName.prefix, (i + 1));
+       startRollNo[i] = readField.ReadFieldValue(fieldName.startRollNo,
+                                                  (i + 1));
+       endRollNo[i] = readField.ReadFieldValue(fieldName.endRollNo, 
+                                                (i + 1));
+       notIncluded[i] = readField.ReadFieldValue(fieldName.notIncluded);
+   }
+}
+
+void RollNoDetails :: WriteClassDetails()
+{
+    outFile.open(CLASS_DETAILS_IN);
+    
+    for(i = 0; i < totalClasses; i++)
+    {
+        outFile << totalClasses << endl
+                << className[i] << endl
+                << subjectName[i] << endl
+                << subjectCode[i] << endl;
+    }
+
+    outFile.close();
+}
+
+void 
