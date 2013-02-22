955d22654cea766e341b5643f2da8692d924bc7c
megha55 <meghasimak@gmail.com>
2013-02-22 14:31:58 +0530

    base class for all pages

---
+++ src/commondetails.h
@@ -0,0 +1,25 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  details.h
+ *
+ *    Description:  Base class for all common variable declaration
+ *
+ *        Version:  0.6
+ *        Created:  Friday 22 February 2013 11:34:29  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  Include local header files
+ *------------------------------------------------------------------*/
+
+#include "pagelayout.h"
+#include "inputfieldnames.h"
+#include "readfieldvalues.h"
