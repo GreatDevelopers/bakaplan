45faed54fcd7dc3a20f6a2e404f844aa9b66ebff
megha55 <meghasimak@gmail.com>
2013-03-05 10:36:09 +0530

    project detail page

---
+++ src/projectdetail.h
@@ -0,0 +1,46 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  projectdetail.h
+ *
+ *    Description:  ProjectDetail class definition for getting project
+ *                  name and project ID
+ *
+ *        Version:  0.6
+ *        Created:  Friday 01 March 2013 02:25:42  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, GreatDevelopers
+ *                  https://github.com/GreatDevelopers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  Include Required header files
+ *------------------------------------------------------------------*/
+
+#include "inputdetail.h"
+
+/**
+ * ===================================================================
+ *        Class:  ProjectDetail
+ *  Description:  For getting project name and giving unique project
+ *                ID.
+ * ===================================================================
+ */
+
+class ProjectDetail
+{
+    protected:
+        
+
+    public:
+        /** ProjectDetail Consructor */
+        ProjectDetail();
+
+        void ProjectDetailPage(); 
+
+};
