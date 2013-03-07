5473cdd09c677246fcdada76f10aa8487b4fe554
megha55 <meghasimak@gmail.com>
2013-03-07 13:39:49 +0530

    projectdetail.html

---
+++ src/projectdetail-main.cpp
@@ -0,0 +1,36 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  projectdetail-main.cpp
+ *
+ *    Description:  For creating projectdetail.html page using object
+ *                  of ProjectDetail Class.
+ *
+ *        Version:  0.6
+ *        Created:  Thursday 07 March 2013 12:48:06  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, GreatDevelopers
+ *                  https://github.com/GreatDevelopers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------\n
+ *  Include projectdetail.h file \n
+ *------------------------------------------------------------------*/
+
+#include "projectdetail.h"
+
+/**-------------------------------------------------------------------\n
+ *  Main Method \n
+ *------------------------------------------------------------------*/
+
+int main(void)
+{
+    ProjectDetail projectDetail;
+    projectDetail.AuthorizeUser();
+    return 0;
+}
