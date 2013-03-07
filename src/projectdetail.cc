e67580f27c0321a0af0a051974fe0e6dfe74639b
megha55 <meghasimak@gmail.com>
2013-03-07 11:53:18 +0530

    ProjectDetail class definition

---
+++ src/projectdetail.cc
@@ -0,0 +1,53 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  projectdetail.cc
+ *
+ *    Description:  Definition of member functions of ProjectDetail 
+ *                  class. It's for getting project details and if
+ *                  user login details are in valid then login.html
+ *                  page will be loaded again.
+ *
+ *        Version:  0.6
+ *        Created:  Wednesday 06 March 2013 04:50:25  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, GreatDevelopers
+ *                  https://github.com/GreatDevelopers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------\n
+ *  Include projectdetail.h for definition of class ProjectDetail \n
+ *------------------------------------------------------------------*/
+
+#include "projectdetail.h"
+
+/**
+ *--------------------------------------------------------------------\n
+ *       Class:  ProjectDetail \n
+ *      Method:  ProjectDetail :: ProjectDetail() \n
+ * Description:  Constructor of ProjectDetail \n
+ *--------------------------------------------------------------------
+ */
+
+ProjectDetail :: ProjectDetail()
+{
+
+}
+
+/**
+ *--------------------------------------------------------------------\n
+ *       Class:  ProjectDetail \n
+ *      Method:  ProjectDetail :: AuthorizeUser() \n
+ * Description:  Checking user login details valid or invalid \n
+ *--------------------------------------------------------------------
+ */
+
+void ProjectDetail :: AuthorizeUser()
+{
+    
+}
