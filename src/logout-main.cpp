fd9ce505893ea043869e38b15ba743eda3578c2c
megha55 <meghasimak@gmail.com>
2013-03-12 03:43:20 +0530

    logout page

---
+++ src/logout-main.cpp
@@ -0,0 +1,37 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  logout-main.cpp
+ *
+ *    Description:  logout.html page
+ *
+ *        Version:  0.6
+ *        Created:  Monday 11 March 2013 02:34:45  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, GreatDevelopers
+ *                  https://github.com/GreatDevelopers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------\n
+ *  Include login.h header file \n
+ *------------------------------------------------------------------*/
+
+#include "login.h"
+
+
+/**-------------------------------------------------------------------\n
+ *  Main function \n
+ *------------------------------------------------------------------*/
+
+int main(void)
+{
+    Login logout;
+    logout.Logout();
+
+    return 0;
+}
