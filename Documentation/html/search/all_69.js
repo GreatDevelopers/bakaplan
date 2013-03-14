943e30f2daaff2ec8bbdf09b164e33e4544fff52
megha55 <meghasimak@gmail.com>
2013-03-14 23:57:39 +0530

    Documentation

--- Documentation/html/search/all_69.js
+++ Documentation/html/search/all_69.js
@@ -3,7 +3,7 @@ var searchData=
   ['inputdetail',['InputDetail',['../classInputDetail.html',1,'InputDetail'],['../classInputDetail.html#ab53655b14d922eb32b5d5d06c702e497',1,'InputDetail::InputDetail()']]],
   ['inputfield',['InputField',['../classPageStructureMaker.html#a3547027801e298307527f1e934787b13',1,'PageStructureMaker::InputField(string type, string name, int nameNo, string value)'],['../classPageStructureMaker.html#a928ea6f84a8f7833c128034068a4b9a7',1,'PageStructureMaker::InputField(string type, string name, string value)']]],
   ['inputfieldname',['InputFieldName',['../classInputFieldName.html',1,'InputFieldName'],['../classInputFieldName.html#a185ace189c56eed847111756922e5158',1,'InputFieldName::InputFieldName()']]],
-  ['insertclassdetail',['InsertClassDetail',['../classDatabase.html#ad0052cd5a48f5ba3233395096ba6e422',1,'Database']]],
+  ['insertclassdetail',['InsertClassDetail',['../classDatabase.html#a45deae7ea496fccefb9bccd75ea5578a',1,'Database']]],
   ['insertexamdetail',['InsertExamDetail',['../classDatabase.html#a0a1ec932aafeef96637d5ee68ac7898b',1,'Database']]],
   ['insertprojectname',['InsertProjectName',['../classDatabase.html#aae19d9dac16779519f94cc68e29cb57d',1,'Database']]],
   ['insertquery',['InsertQuery',['../classDatabase.html#a63d8c1af7507b1dcdc81a411f9a0b4b4',1,'Database::InsertQuery(string query)'],['../classDatabase.html#a513326ea43b2455177731fbc92c4bf4f',1,'Database::InsertQuery(string column, string value, string table)']]],
--- Documentation/html/search/functions_72.js
+++ Documentation/html/search/functions_72.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['readclassdetails',['ReadClassDetails',['../classRollNoDetail.html#a941f09a9307b97c7d3655e68815954c3',1,'RollNoDetail']]],
+  ['readclassdetail',['ReadClassDetail',['../classRollNoDetail.html#a9ee6a79ebf8f72d0fa488216f645e19c',1,'RollNoDetail']]],
   ['readfieldvalue',['ReadFieldValue',['../classReadInputField.html#a7f6e49b47412649644cc644927ccc682',1,'ReadInputField::ReadFieldValue(string fieldName)'],['../classReadInputField.html#accf7ceba77721a35968c69268e4e559e',1,'ReadInputField::ReadFieldValue(string fieldName, int fieldNo)']]],
   ['readinputfield',['ReadInputField',['../classReadInputField.html#aae743343381035c28a3a0111fc353c7f',1,'ReadInputField']]],
   ['readlogindetail',['ReadLoginDetail',['../classLogin.html#ab5bc65de431f277f15a3b423ad915808',1,'Login']]],
