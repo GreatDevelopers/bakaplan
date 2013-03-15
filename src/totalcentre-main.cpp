87aabd16cfbcbcbc5883dcf343af01df77fb0733
megha55 <meghasimak@gmail.com>
2013-03-15 18:39:33 +0530

    totalcentre.html page

---
+++ src/totalcentre-main.cpp
@@ -0,0 +1,36 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  totalcentre-main.cpp
+ *
+ *    Description: For totalcentres for rooms 
+ *
+ *        Version:  0.6
+ *        Created:  Thursday 14 March 2013 09:07:26  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, GreatDevelopers
+ *                  https://github.com/GreatDevelopers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------\n
+ *  Include roomdetail.h \n
+ *------------------------------------------------------------------*/
+
+#include "roomdetail.h"
+
+/**-------------------------------------------------------------------\n
+ *  Main method  \n
+ *------------------------------------------------------------------*/
+
+int main(void)
+{
+    RoomDetail centre;
+    centre.TotalCentrePage();
+
+    return 0;
+}
