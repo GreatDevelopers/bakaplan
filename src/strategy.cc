876968c5b56cd021cdeea1d6b89dbb1c5432c9ef
megha55 <meghasimak@gmail.com>
2013-03-17 23:51:04 +0530

    for strategy.html, reading room details amd save into DB

---
+++ src/strategy.cc
@@ -0,0 +1,87 @@
+/**-------------------------------------------------------------------
+ *
+ *       \file       strategy.cc
+ *
+ *       \brief      For defnition of class member functions of
+ *                   Strategy Class.
+ *
+ *       \version    0.6
+ *       \date       Sunday 17 March 2013 11:17:20  IST\n
+ *       Compiler    gcc
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ *
+ *------------------------------------------------------------------*/
+
+/**
+ *  Include strategy.h 
+ */
+#include "strategy.h"
+
+/**
+ *      \class  Strategy
+ *      \fn     Strategy :: Strategy()
+ *      \brief  Constructor
+ *      \param
+ */
+
+Strategy :: Strategy()
+{
+    // constructor
+}
+
+/**
+ *      \class  Strategy
+ *      \fn     Strategy :: ReadRoomDetail()
+ *      \brief  Read room details from previous page and store them
+ *              into database
+ *      \param  
+ */
+
+void Strategy :: ReadRoomDetail()
+{
+    projectID = readField.ReadFieldValue(fieldName.projectID);
+    
+    database.SelectSum("TotalRooms", "TotalRooms", projectID, temp);
+
+    totalRooms = StringToInt(temp);
+
+    cout << totalRooms;
+}
+
+/**
+ *      \class  Strategy
+ *      \fn     Strategy :: StrategyPage()
+ *      \brief  Strategy page got taking i/p from user
+ *      \param
+ */
+
+void Strategy :: StrategyPage()
+{
+    ContextType();
+
+    Header("Strategy");
+    
+    DivStart("strategy", "");
+    
+    ReadRoomDetail();
+
+    DivEnd();
+
+    Footer();
+}
+
+/**
+ *      \class  Strategy
+ *      \fn     Strategy :: ~Strategy()
+ *      \brief  Desctuctor
+ *      \param
+ */
+
+Strategy :: ~Strategy()
+{
+    // Destructor
+}
---
+++ src/strategy.h
@@ -0,0 +1,53 @@
+/**
+ *       \file       strategy.h
+ *
+ *       \brief      Strategy class for taking strategy detail from
+ *                   user. 
+ *
+ *       \version    0.6
+ *       \date       Sunday 17 March 2013 10:54:33  IST\n
+ *       Compiler    gcc
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+/**
+ *  Include inputdetail.h file for basic variables and functions
+ */
+
+#include "inputdetail.h"
+
+
+/**
+ *      \class  Strategy
+ *      \brief  Strategy class for taking strategy option from user
+ *              and storing room details into database.
+ */
+
+class Strategy : public InputDetail
+{
+    protected:
+        
+        string centreName[MIN_SIZE],
+               roomNo[MIN_SIZE],
+               rows[MIN_SIZE],
+               columns[MIN_SIZE];
+
+        int totalRooms;
+
+    public:
+        /** Constructor */
+        Strategy();
+
+        /** Reading room details and storing them into database */
+        void ReadRoomDetail();
+
+        /** strategy page */
+        void StrategyPage();
+    
+        /** destuctor */
+        ~Strategy();
+};
