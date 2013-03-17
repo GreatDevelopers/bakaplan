6b18fc95eb910a25fca449a939949db8f7e85c70
megha55 <meghasimak@gmail.com>
2013-03-17 13:46:02 +0530

    for centredetail.html

---
+++ src/centredetail-main.cpp
@@ -0,0 +1,32 @@
+/**
+ *       \file      centredetail-main.cpp
+ *
+ *       \brief     Centre Detail like centre name and total rooms
+ *
+ *       \version   0.6
+ *       \date      Sunday 17 March 2013 11:11:53  IST
+ *       Compiler  gcc
+ *
+ *       \author    Mandeep Kaur, meghasimak@gmail.com
+ *        License  GNU General Public License
+ *      \copyright  Copyright (c) 2013, GreatDevelopers
+ *                  https://github.com/GreatDevelopers
+ */
+
+/**-------------------------------------------------------------------\n
+ *  Include roomdetail.h \n
+ *------------------------------------------------------------------*/
+
+#include "roomdetail.h"
+
+/**-------------------------------------------------------------------\n
+ *  Main method \n
+ *------------------------------------------------------------------*/
+
+int main(void)
+{
+    RoomDetail centre;
+    centre.CentreDetailPage();
+
+    return 0;
+}
