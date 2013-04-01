cd1b540f2c84ab5425352fb004d807076c8781e0
megha55 <meghasimak@gmail.com>
2013-04-02 02:52:49 +0530

    main.cpp file for checking program is working or not

---
+++ bakaplan/src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ *       \file       main.cpp
+ *
+ *       \brief      Main Method
+ *
+ *       \version    0.6
+ *       \date       Sunday 31 March 2013 08:13:40  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+//#include "readinput.cc"
+#include "header/arrangerollno.h"
+
+int main(void)
+{
+    ArrangeRollNo rNo;
+//    rNo.expandRNo.ExpandRollNos("1");
+    rNo.Main("1");
+
+    return 0;
+}
