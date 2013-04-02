7c355fe851b54cc5af78959463359fef286e1aec
megha55 <meghasimak@gmail.com>
2013-04-02 21:20:08 +0530

    strategy file name added

--- bakaplan/src/header/filename.h
+++ bakaplan/src/header/filename.h
@@ -28,6 +28,7 @@ const char ROLLNO_DETAIL[FILE_SIZE]  = "rollno-detail-";
 const char DATESHEET[FILE_SIZE]      = "datesheet-";
 const char EXAM_DETAIL[FILE_SIZE]    = "exam-detail-";
 const char ROOM_DETAIL[FILE_SIZE]    = "room-detail-";
+const char STRATEGY[FILE_SIZE]       = "strategy-";
 
 const char EXPAND_ROLLNO[FILE_SIZE]  = "expand-rollno-";
 const char ARRANGE_ROLLNO[FILE_SIZE] = "arrange-rollno-";
