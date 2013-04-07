2d0d51fde333375ae56425cd00a9c5e7b488a5e1
megha55 <meghasimak@gmail.com>
2013-04-07 21:30:31 +0530

    rollno.html

---
+++ frontend/src/rollno.cc
@@ -0,0 +1,41 @@
+/**
+ *       \file       rollno.cc
+ *
+ *       \brief      RollNo detail Class definition
+ *
+ *       \version    0.6
+ *       \date       Sunday 07 April 2013 07:53:57  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+/**
+ *  include rollno.h 
+ */
+#include "header/rollno.h"
+
+/**
+ *      \class  RollNoDetail
+ *      \fn     RollNoDetail :: RollNoDetail()
+ *      \brief  Constructor
+ */
+
+RollNoDetail :: RollNoDetail()
+{
+    // constructor
+}
+
+/**
+ *      \class  RollNoDetail
+ *      \fn     RollNoDetail :: ~RollNoDetail()
+ *      \brief  Destructor
+ */
+
+RollNoDetail :: ~RollNoDetail()
+{
+    // destructor
+}
