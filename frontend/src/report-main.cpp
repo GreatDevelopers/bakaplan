1ef3f4a150db1602759946893074546b81eecc7a
megha55 <meghasimak@gmail.com>
2013-04-07 21:31:24 +0530

    report.html

---
+++ frontend/src/report-main.cpp
@@ -0,0 +1,33 @@
+/**
+ *       \file       report-main.cpp
+ *
+ *       \brief      main method
+ *
+ *       \version    0.6
+ *       \date       Sunday 07 April 2013 09:00:37  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+/**
+ *  include header file 
+ */
+
+#include "header/report.h"
+
+/** 
+ *      \fn     main(void)
+ *      \brief  Main method for creating.html page
+ */
+
+int main(void)
+{
+
+    Report report;
+
+    return 0;
+}
---
+++ frontend/src/report.cc
@@ -0,0 +1,26 @@
+/**
+ *       \file       report.cc
+ *
+ *       \brief      func. definition
+ *
+ *       \version    0.6
+ *       \date       Sunday 07 April 2013 08:28:46  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+#include "header/report.h"
+
+Report :: Report()
+{
+    // constructor
+}
+
+Report :: ~Report()
+{
+    // destructor
+}
