9f54ad2aeadc4d8baebe7624ed3635dd56bcf707
megha55 <meghasimak@gmail.com>
2013-04-07 21:32:54 +0530

    datesheet.html

---
+++ frontend/src/datesheet.cc
@@ -0,0 +1,41 @@
+/**
+ *       \file       datesheet.cc
+ *
+ *       \brief      Function definition of datesheet class
+ *
+ *       \version    0.6
+ *       \date       Sunday 07 April 2013 07:59:55  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+/**
+ *  include datesheet.h 
+ */
+#include "header/datesheet.h"
+
+/**
+ *      \class  DateSheet
+ *      \fn     DateSheet :: DateSheet()
+ *      \brief  Constructor
+ */
+
+DateSheet :: DateSheet()
+{
+    // constructor
+}
+
+/**
+ *      \class  DateSheet
+ *      \fn     DateSheet :: ~DateSheet()
+ *      \brief  DEstructor
+ */
+
+DateSheet :: ~DateSheet()
+{
+    // destructor
+}
