1418d5363be05e6bdd7e35952c09fb5bec92babe
megha55 <meghasimak@gmail.com>
2013-04-07 21:33:17 +0530

    room.html

---
+++ frontend/src/header/room.h
@@ -0,0 +1,36 @@
+/**
+ *       \file       room.h
+ *
+ *       \brief      RoomDetail class
+ *
+ *       \version    0.6
+ *       \date       Sunday 07 April 2013 08:14:54  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+#ifndef ROOM_H
+#define ROOM_H
+
+/**
+ *  include inpudetail.h 
+ */
+
+#include "inputdetail.h"
+
+class RoomDetail : public InputDetail
+{
+    protected:
+
+    public:
+
+        RoomDetail();
+
+        ~RoomDetail();
+};
+
+#endif
---
+++ frontend/src/room.cc
@@ -0,0 +1,29 @@
+/**
+ *       \file       room.cc
+ *
+ *       \brief      RoomDetail class definition
+ *
+ *       \version    0.6
+ *       \date       Sunday 07 April 2013 08:17:36  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+/**
+ *  Include room.h 
+ */
+#include "header/room.h"
+
+RoomDetail :: RoomDetail()
+{
+    // constructor
+}
+
+RoomDetail :: ~RoomDetail()
+{
+    // destructor
+}
