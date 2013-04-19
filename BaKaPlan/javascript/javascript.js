d04bcfd350a93eabda3d085ae1a531c053ea59d4
megha55 <meghasimak@gmail.com>
2013-04-19 10:25:19 +0530

    Javascript fun. to add row in table

--- BaKaPlan/javascript/javascript.js
+++ BaKaPlan/javascript/javascript.js
@@ -1,11 +1,22 @@
+// Function for adding row in table
 function addRow(tableID, totalID, field) 
 {
     try
     {
+//        if (typeof tableNo == 'undefined')
+//                tableNo = 0;
+
         var table = document.getElementById(tableID);
         var total = document.getElementById(totalID).value;
-        var classField = new Array("ClassName", "SubjectName", "SubjectCode");
-
+        var classField = new Array("ClassName", "SubjectName", 
+                                   "SubjectCode");
+        var DateField = new Array("Date", "ExamCode");
+/*        var RoomField = new Array("CentreName", "RoomNo", 
+                                  "Rows", "Columns");
+        var ExamField = new Array("ExamName", "ExamSession",
+                                  "ExamVenue", "ExamTime");
+        var StrategyFiels = new Array("Strategy");
+*/
         var newTotal = parseInt(total) + 1;
  
         var rowCount = table.rows.length;
@@ -30,6 +41,24 @@ function addRow(tableID, totalID, field)
                         newcell.childNodes[0].id = classField[i] 
                                                      + newTotal;
                     }
+                    else if(field == "date")
+                    {
+                        newcell.childNodes[0].name = DateField[i] 
+                                                     + newTotal;
+                        newcell.childNodes[0].id = DateField[i] 
+                                                     + newTotal;
+                    }
+/*                    else if(field == "room")
+                    {
+                        var fn = classField[i] + tableID + newTotal;
+                        newcell.childNodes[0].name = fn;
+                        newcell.childNodes[0].id = fn; 
+
+                    }*/
+                    else
+                    {
+
+                    }
                     break;
                 case "checkbox":
                     newcell.childNodes[0].checked = false;
@@ -46,22 +75,86 @@ function addRow(tableID, totalID, field)
         alert(e);
     }
 }
- 
-function fieldName(field) 
+
+// Function for adding row in table
+function addRow(tableID, totalID, field, tableNo) 
 {
-    try 
+    try
     {
-        var classField = new Array("ClassName", "SubjectName", "SubjectCode");
-        if(field == "class")
-            newcell.childNodes[0].name = classField[i] + newTotal;
+//        if (typeof tableNo == 'undefined')
+//                tableNo = 0;
+
+        var table = document.getElementById(tableID);
+        var total = document.getElementById(totalID).value;
+/*        var classField = new Array("ClassName", "SubjectName", 
+                                   "SubjectCode");
+        var DateField = new Array("Date", "ExamCode");*/
+        var RoomField = new Array("CentreName", "RoomNo", 
+                                  "Rows", "Columns");
+        var ExamField = new Array("ExamName", "ExamSession",
+                                  "ExamVenue", "ExamTime");
+        var StrategyFiels = new Array("Strategy");
+
+        var newTotal = parseInt(total) + 1;
+ 
+        var rowCount = table.rows.length;
+        var row = table.insertRow(rowCount);
+ 
+        var colCount = table.rows[0].cells.length;
+ 
+        for(var i=0; i<colCount; i++) 
+        {
+            var newcell = row.insertCell(i);
+ 
+            newcell.innerHTML = table.rows[1].cells[i].innerHTML;
+            //alert(newcell.childNodes);
+            switch(newcell.childNodes[0].type) 
+            {
+                case "text":
+                    newcell.childNodes[0].value = "";
+/*                    if(field == "class")
+                    {
+                        newcell.childNodes[0].name = classField[i] 
+                                                     + newTotal;
+                        newcell.childNodes[0].id = classField[i] 
+                                                     + newTotal;
+                    }
+                    else if(field == "date")
+                    {
+                        newcell.childNodes[0].name = DateField[i] 
+                                                     + newTotal;
+                        newcell.childNodes[0].id = DateField[i] 
+                                                     + newTotal;
+                    }*/
+                    if(field == "room")
+                    {
+                        var fn = RoomField[i] + tableNo + newTotal;
+                        newcell.childNodes[0].name = fn;
+                        newcell.childNodes[0].id = fn; 
+
+                    }
+                    else
+                    {
+
+                    }
+                    break;
+                case "checkbox":
+                    newcell.childNodes[0].checked = false;
+                    break;
+                case "select-one":
+                    newcell.childNodes[0].selectedIndex = 0;
+                    break;
+            }
+         }
+         document.getElementById(totalID).value = newTotal;
     }
-    catch(e) 
+    catch(e)
     {
         alert(e);
     }
 }
-
-function removeRow(tableID, totalID)
+// Function for deleting row from table
+function deleteRow(tableID, totalID)
 {
     try
     {        
