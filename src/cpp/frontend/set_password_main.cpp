28fc3ee130a84242081cdaacae2075f00a61502e
megha55 <meghasimak@gmail.com>
2013-12-02 02:47:14 +0530

    set password script

---
+++ src/cpp/frontend/set_password_main.cpp
@@ -0,0 +1,23 @@
+/**
+ *       \file       set_password_main.cpp
+ *
+ *       \brief      Set Password for new user \n
+ *
+ *       \compiler   g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com \n
+ *       \license    GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+#include "header/user.h"
+
+int main(void)
+{
+    User user;
+
+    user.SetPassword();
+
+    return 0;
+}
