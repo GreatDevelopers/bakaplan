59f7a9b5ae9cf9294e95ea32a921b8807c202d61
simakmandeep@gmail.com <meghasimak@gmail.com>
2014-07-20 20:13:08 +0530

    css class names

---
+++ src/cpp/frontend/header/css_class.h
@@ -0,0 +1,48 @@
+/**
+ *       \file       css_class.h
+ *
+ *       \brief      Map for CSS classes used for form elements and other
+ *                   styling
+ *
+ *       \compiler   g++\n
+ *
+ *       \author     Mandeep Kaur, simakmandeep@gmail.com
+ *       \license    GNU General Public License Version 2\n
+ *       \copyright  Copyright (c) 2014, GreatDevelopers\n
+ *                   https://github.com/GreatDevelopers
+ */
+
+#ifndef  CSS-CLASS_H
+#define  CSS-CLASS_H
+
+#include <iostream>
+#include <map>
+#include <string>
+
+/**
+ *  Defining css map for CSS classes used for form elements and other
+ *  styling
+ */
+
+namespace css
+{
+    std::map<string, string> className = 
+    {
+
+        /* CSS Class Names */
+
+        {"text", "theFormTextInput"},
+        {"button", "theFormButton"},
+        {"label", "theLabel"},
+        {"error", "theError"}
+    };
+}
+
+#endif   /* #ifndef CSS-CLASS_H */
+
+/*
+ * Local Variables:
+ * tab-width: 4
+ * expandtab
+ * ex: shiftwidth=4 tabstop=4
+ */ 
